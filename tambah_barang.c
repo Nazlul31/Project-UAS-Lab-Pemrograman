f837feb45bcf7eef04384f9df54fc12733195465
Nazlul31 <163635933+Nazlul31@users.noreply.github.com>
2024-05-15 17:20:02 +0700

    Update and rename tambah_barang.c to fungsi_tambah_barang.c
    Menambahkan flow control jika nama barang sudah ada dalam daftar

--- tambah_barang.c
+++
@@ -1,9 +0,0 @@
-void tambah_barang() {
-	printf("-----TAMBAH BARANG-----\n");
-    FILE *file = fopen("daftar_barang.txt", "r");
-    struct Barang barang;
-    char nama_barang_baru[50];
-    int found = 0;
-
-    printf("Masukkan nama barang	: ");
-    scanf("%s", nama_barang_baru);
