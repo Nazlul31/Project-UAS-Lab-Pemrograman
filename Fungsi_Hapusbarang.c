65ee83a94d2ee0d1e260991d6b0438c58da3610a
RandyMaulanaa <163530084+RandyMaulanaa@users.noreply.github.com>
2024-05-15 22:56:38 +0700

    ganti fgets ke scanf

--- Fungsi_Hapusbarang.c
+++ Fungsi_Hapusbarang.c
@@ -1,14 +1,10 @@
-   // Fungsi untuk menghapus barang dari daftar
 void hapus_barang() {
     printf("-----HAPUS BARANG-----\n");
     char nama_barang[50];
     int found = 0;
 
     printf("Nama barang: ");
-    getchar();
-    fgets(nama_barang, sizeof(nama_barang), stdin);
-    nama_barang[strcspn(nama_barang, "\n")] ='\0';
-    // scanf("%s", nama_barang);
+    scanf("%s", nama_barang);
 
     FILE *file = fopen("barang_tersedia.txt", "r");
     FILE *temp_file = fopen("temp_barang_tersedia.txt", "w");
@@ -19,7 +15,7 @@ void hapus_barang() {
     while (fscanf(file, "%s %f %d", barang.nama, &barang.harga, &barang.stok) != EOF) {
         if (strcmp(nama_barang, barang.nama) == 0) {
             found = 1;
-            printf("Barang \"%s\" telah dihapus.\n", nama_barang);
+            printf("Berhasil!! \"%s\" telah dihapus\n", nama_barang);
         } else {
             fprintf(temp_file, "%s %.2f %d\n", barang.nama, barang.harga, barang.stok);
         }
@@ -32,5 +28,5 @@ void hapus_barang() {
     rename("temp_barang_tersedia.txt", "barang_tersedia.txt");
 
     if (!found)
-        printf("Barang tidak ditemukan.\n");
+        printf("Barang tidak ditemukan\n");
 }
